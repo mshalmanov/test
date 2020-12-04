6fd0f804292bdce7bd9b2a1622443ceda99a92df
Marat <1203Marat@gmail.com>
2020-12-04 10:24:19 +0600

    change index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Travis!\n');
-}).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:80/');
+}).listen(8080);
